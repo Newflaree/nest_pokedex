257988dfba4f2c678b556c76f6e1e2921092b03c
Camilo López <camiloj.lopez79@gmail.com>
2025-02-19 11:11:51 -0300

    FindOneBy - Buscar por nombre, MongoId o No

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,10 +1,17 @@
+// Nest
 import {
   BadRequestException,
   Injectable,
-  InternalServerErrorException
+  InternalServerErrorException,
+  NotFoundException
 } from '@nestjs/common';
+// Nest Mongoose
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+// Mongoose
+import {
+  isValidObjectId,
+  Model
+} from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
@@ -35,8 +42,25 @@ export class PokemonService {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(term: string) {
+    let pokemon: Pokemon;
+
+    if ( !isNaN( +term ) ) {
+      pokemon = await this.pokemonModel.findOne({ no: term });
+    }
+
+    if ( !pokemon && isValidObjectId( term ) ) {
+      pokemon = await this.pokemonModel.findById( term );
+    }
+
+    if ( !pokemon ) {
+      pokemon = await this.pokemonModel.findOne({ name: term.toLowerCase().trim() });
+    }
+
+    if ( !pokemon )
+      throw new NotFoundException( `Pokemon with id, name or no "${ term }" not found` );
+
+    return pokemon;
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
