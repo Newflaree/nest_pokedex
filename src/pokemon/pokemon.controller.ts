12ad9a1ba469b2b243b2ab291c91133659d71856
Camilo López <camiloj.lopez79@gmail.com>
2025-02-22 16:10:42 -0300

    Paginación de Pokemons

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -6,13 +6,15 @@ import {
   Patch,
   Param,
   Delete,
-  HttpCode,
-  HttpStatus
+  Query,
+  //HttpCode,
+  //HttpStatus
 } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { ParseMongoIdPipe } from 'src/common/pipes/parse-mongo-id.pipe';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Controller('pokemon')
 export class PokemonController {
@@ -25,7 +27,7 @@ export class PokemonController {
   }
 
   @Get()
-  findAll() {
+  findAll( @Query() paginationDto: PaginationDto ) {
     return this.pokemonService.findAll();
   }
 
